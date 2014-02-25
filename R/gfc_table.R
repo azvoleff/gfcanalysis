3dd874a200567e7bb0459264c89e18ba252889d9
Alex Zvoleff <azvoleff@gmail.com>
2014-02-25 10:53:01 -0500

    Add stubs of analysis scripts.

---
+++ R/gfc_table.R
@@ -0,0 +1,12 @@
+#' xxx
+#'
+#' yyy
+#'
+#' @export
+#' @param aoi an Area of Interest (AOI) as a \code{SpatialPolygons*} object.  
+#' If the AOI is not in the WGS84 geographic coordinate system, it will be 
+#' reprojected to WGS84.
+#' @examples
+#' #TODO: Add examples
+gfc_table <- function(aoi, output_path) {
+}
