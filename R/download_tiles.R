5071184b7b2d755f4d6449c25764705cd153f342
Alex Zvoleff <azvoleff@gmail.com>
2014-02-25 09:36:36 -0500

    Finalize tile download code.

--- R/download_tiles.R
+++ R/download_tiles.R
@@ -1,33 +1,25 @@
+#' @importFrom RCurl getUrl
+#' @importFrom stringr str_extract
 verify_download <- function(tile_url, local_path) {
-    cksum_file <- tempfile()
-    ret_code <- download.file(gsub('\\.tar\\.gz$', '.cksum', tile_url), 
-                              cksum_file, mode="w", quiet=TRUE)
-    if (ret_code != 0) {
-        message(paste('Warning: problem downloading cksum for', local_path))
-        return(1)
+    header <- getURL(tile_url, nobody=1L, header=1L)
+    header <- strsplit(header, "\r\n")[[1]]
+    content_length <- header[grepl('Content-Length: ', header)]
+    remote_size <- as.numeric(str_extract(content_length, '[0-9]+'))
+    local_size <- file.info(local_path)$size
+    if (remote_size != local_size) {
+        return(3)
     } else {
-        # TODO: Check return code and handle accordingly
-        # The first element is the cksum, second is the expected file size in 
-        # bytes, and the third is the filename
-        espa_checksum <- scan(cksum_file, what=c('integer', 'integer', 
-                                                 'character'), quiet=TRUE)
-        unlink(cksum_file)
-        local_size <- file.info(local_path)$size
-        if (espa_checksum[2] != local_size) {
-            return(3)
-        } else {
-            return(0)
-        }
+        return(0)
     }
 }
 
-download_tile <- function(tile_url, output_path) {
-    ret_code <- download.file(tile_url, output_path, mode="wb")
+download_tile <- function(tile_url, local_path) {
+    ret_code <- download.file(tile_url, local_path, mode="wb")
     if (ret_code != 0) {
-        message(paste('Warning: problem downloading', output_path))
+        message(paste('Warning: problem downloading', local_path))
         return(1)
-    } else if (verify_download(tile_url, output_path) != 0) {
-        message(paste("Warning: checksum mismatch on", output_path))
+    } else if (verify_download(tile_url, local_path) != 0) {
+        message(paste("Warning: verification failed on", local_path))
         return(2)
     } else {
         return(0)
@@ -40,36 +32,66 @@ download_tile <- function(tile_url, output_path) {
 #' verifying that are not present locally.
 #' @export
 #' @importFrom stringr str_extract
-#' @param gfc_tiles \code{SpatialPolygonsDataFrame} with Global Forest Change 
+#' @param tiles \code{SpatialPolygonsDataFrame} with Global Forest Change 
 #' product tiles to download, as calculated by the \code{calc_gfc_tiles} 
 #' function.
 #' @param output_folder the folder to save output data in
 #' @return used for the side effect of downloading GFC tiles
-download_tiles <- function(gfc_tiles, output_folder) {
+#' @examples
+#' output_folder <- 'H:/Data/TEAM/GFC_Product'
+#' tiles <- calc_gfc_tiles(test_poly, 'H:/Data/TEAM/GFC_Product')
+#' download_tiles(tiles, output_folder)
+download_tiles <- function(tiles, output_folder) {
+    message(paste(nrow(tiles), 'tiles to download/check.'))
     successes <- 0
     failures <- 0
     skips <- 0
-    message(paste('Found', length(tile_urls), 'tiles to download.'))
-    for (n in 1:length(tile_urls)) {
-        tile_url <- tile_urls[n]
-        img_file <- basename(tile_url)
-        output_path <- file.path(output_folder, img_file)
-        if (file.exists(output_path)) {
-            if (verify_download(tile_url, output_path)) {
-                message(paste(img_file, 'exists but has bad checksum - re-downloading file'))
-            } else {
-                message(paste(img_file, 'exists and has good checksum - skipping download'))
-                skips <- skips + 1
-                next
-            }
-        }
-        if (download_tile(tile_url, output_path) == 0) {
-            successes <- successes + 1
-        } else {
-            failures <- failures + 1
-        }
 
+    for (n in 1:length(tiles)) {
+        gfc_tile <- tiles[n,]
+        min_x <- bbox(gfc_tile)[1, 1]
+        max_y <- bbox(gfc_tile)[2, 2]
+        if (min_x < 0) {
+            min_x <- paste0(sprintf('%03i', abs(min_x)), 'W')
+        } else {
+            min_x <- paste0(sprintf('%03i', min_x), 'E')
+        }
+        if (max_y < 0) {
+            max_y <- paste0(sprintf('%02i', abs(max_y)), 'S')
+        } else {
+            max_y <- paste0(sprintf('%02i', max_y), 'N')
+        }
+        file_root <- 'Hansen_GFC2013_'
+        file_suffix <- paste0('_', max_y, '_', min_x, '.tif')
+        filenames <- paste0(file_root, c('treecover2000', 'loss', 'gain', 
+                                        'lossyear', 'datamask', 'first', 
+                                        'last'),
+                            file_suffix)
+        tile_urls <- paste0('http://commondatastorage.googleapis.com/earthenginepartners-hansen/GFC2013/',
+                      filenames)
+        local_paths <- file.path(output_folder, filenames)
+
+        for (i in 1:length(filenames)) {
+            tile_url <- tile_urls[i]
+            local_path <- local_paths[i]
+            if (file.exists(local_path)) {
+                if (verify_download(tile_url, local_path)) {
+                    message(paste(local_path, "exists but doesn't match remote - re-downloading file"))
+                } else {
+                    message(paste(local_path, 'exists and matches remote - skipping download'))
+                    skips <- skips + 1
+                    next
+                }
+            }
+            if (download_tile(tile_url, local_path) == 0) {
+                successes <- successes + 1
+            } else {
+                failures <- failures + 1
+            }
+
+        }
     }
+
     message(paste(successes, "tile(s) succeeded,", skips, "tile(s) skipped,", 
                 failures, "tile(s) failed."))
 }
