9ff6e8c99e5b9a10b52b163c984947a83d085b54
Alex Zvoleff <azvoleff@gmail.com>
2014-03-05 18:11:46 -0500

    Update recoding (simplify).

--- R/annual_stack.R
+++ R/annual_stack.R
@@ -1,11 +1,3 @@
-recode_gfc <- function(this_year, floss, fgain, datamask) {
-    this_year[(this_year == 0) & fgain] <- 4 # gain (no years attached to gain)
-    this_year[fgain & floss] <- 5 # loss and gain
-    this_year[datamask == 2] <- 6 # water
-    this_year[datamask == 0] <- 0 # no data
-    return(this_year)
-}
-
 #' Generate an annual stack of forest change from GFC product
 #'
 #' Uses the GFC data output from \code{\link{extract_gfc}} to make an 
@@ -42,23 +34,24 @@ annual_stack <- function(gfc, forest_threshold=25) {
     for (n in 1:length(layer_names)) {
         if (n == 1) {
             # Code forest as 1, non-forest as 2
-            this_year <- (gfc$treecover2000 > forest_threshold) & (gfc$datamask == 1)
-            this_year[this_year != 1] <- 2
-            this_year <- this_year
+            this_year <- gfc$treecover2000 > forest_threshold
+            this_year[this_year == 0] <- 2
+            # Code missing data
+            this_year[gfc$datamask == 0] <- 0
+            # Code gain as 4 (no years are attributed to gain). Gain can only 
+            # occur on initially non-forested pixels (this_year == 2) where 
+            # loss does not also occur (loss == 0)
+            this_year[(this_year == 2) & gfc$gain & (gfc$loss == 0)] <- 4
+            # Code forest loss/gain, and water
+            this_year[gfc$gain & (gfc$loss != 0)] <- 5 # loss and gain
+            this_year[gfc$datamask == 2] <- 6 # water
         } else {
             this_year <- raster(out, layer=(n-1))
         }
-        # First code forest loss
+        # Code forest loss
         this_year[(this_year == 1) & gfc$loss & gfc$lossyear == n] <- 3 # loss
-        # Now code forest gain, loss/gain, water, and no data
-        this_year <- overlay(this_year,
-                             gfc$loss, 
-                             gfc$gain, 
-                             gfc$datamask, 
-                             fun=recode_gfc, datatype='INT1U')
         names(this_year) <- layer_names[n]
         out <- addLayer(out, this_year)
-
     }
     out <- setZ(out, seq(as.Date('2000-1-1'), as.Date('2012-1-1'), by='year'))
     return(out)
