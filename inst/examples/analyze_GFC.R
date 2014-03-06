982d16d88698ccbc188b3de2f8881198ff63485e
Alex Zvoleff <azvoleff@gmail.com>
2014-03-06 14:12:55 -0500

    Add example scripts.

---
+++ inst/examples/analyze_GFC.R
@@ -0,0 +1,71 @@
+###############################################################################
+# This script shows an example of how to use the "gfcanalysis" R package put 
+# together by Alex Zvoleff (azvoleff@conservation.org) for working with the 
+# Hansen et al. 2013 Global Forest Change dataset. Contact Alex if you notice 
+# any issues or have problems using the package.
+# 
+# NOTE: the gfcanalysis package must be installed before this script will run.  
+# Run the "install_gfcanalysis.R" script to install/update the gfcanalysis 
+# package.
+###############################################################################
+
+# Load the gfcanalysis package
+library(gfcanalysis)
+# Load 'rgdal' package, which is used to read/write shapefiles and rasters
+library(rgdal)
+# Load 'raster' package, which is used to read/write/analyze rasters
+library(rgdal)
+
+# Indicate where we want to save GFC tiles downloaded from Google. For any 
+# given AOI, the script will first check to see if these tiles are available 
+# locally (in the below folder) before downloading them from the server - so I 
+# recommend storing ALL of your GFC tiles in the same folder.
+output_folder <- 'C:/Users/azvoleff/Desktop/gfcanalysis_demo'
+
+# Set the threshold for forest/non-forest based on the treecover2000 layer in 
+# the GFC product
+forest_threshold <- 90
+
+###############################################################################
+# Downloading data from Google server for a given AOI
+###############################################################################
+
+# Load a demo AOI from the P drive - notice that first we specify the folder 
+# the shapefile is in, and then the name of the shapefile without the '.shp'
+aoi <- readOGR('P:/Alex Zvoleff/gfcanalysis_demo', 'ZOI_NAK_2012')
+
+# Calculate the google server URLs for the tiles needed to cover the AOI
+tiles <- calc_gfc_tiles(aoi)
+# Check to see if these tiles are already present locally, and download them if 
+# they are not.
+download_tiles(tiles, output_folder, first_and_last=FALSE)
+# Extract the GFC data for this AOI from the downloaded GFC tiles, mosaicing 
+# multiple tiles as necessary (if needed to cover the AOI).
+gfc_data <- extract_gfc(aoi, output_folder)
+# Save the output data to a GeoTIFF (can also save in ENVI format, Erdas 
+# format, etc.)
+writeRaster(gfc_data, filename='test_gfc_extract.tif')
+
+###############################################################################
+# Performing thresholding and calculating basic statistics
+###############################################################################
+
+# Calculate annual statistics on forest loss/gain
+gfc_stats <- gfc_stats(aoi, gfc_data, forest_threshold=forest_threshold)
+# Save these statistics to CSV files for use in Excel, etc.
+write.csv(gfc_stats$loss_table, file='test_gfc_extract_losstable.csv', row.names=FALSE)
+write.csv(gfc_stats$gain_table, file='test_gfc_extract_gaintable.csv', row.names=FALSE)
+
+# Calculate and save a thresholded version of the GFC product
+gfc_thresholded <- threshold(gfc_data, forest_threshold=forest_threshold)
+writeRaster(gfc_thresholded, filename='test_gfc_extract_thresholded.tif')
+
+# Calculate and save a thresholded annual layer stack from the GFC product 
+# (useful for simple visualizations, etc.)
+gfc_thresholded_annual <- annual_stack(gfc_data, forest_threshold=forest_threshold)
+writeRaster(gfc_thresholded_annual, filename='test_gfc_extract_thresholded_annual.tif')
+
+# Save a simple visualization of the thresholded annual layer stack (this is 
+# just an example, and is using the data in WGS84. The data should be projected 
+# for this).
+animate_annual(aoi, gfc_thresholded_annual)
---
+++ inst/examples/install_gfcanalysis.R
@@ -0,0 +1,16 @@
+###############################################################################
+# This script installs the "gfcanalysis" R package created by Alex Zvoleff 
+# (azvoleff@conservation.org) for working with the Hansen et al. 2013 Global 
+# Forest Change dataset.
+#
+# After installing the package, see the "analyze_gfc.R" script for an example 
+# of how to use the package.
+################################################################################
+
+# Install the devtools package (if not already installed). devtools is used to 
+# install Alex's "gfcanalysis" package.
+if (!require(devtools)) install.packages("devtools")
+
+# Install Alex's gfcanalysis package - you only need to run this line once.
+install_github('azvoleff/gfcanalysis')
+
