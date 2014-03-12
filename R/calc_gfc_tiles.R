9188de79369b780b17566345431f0c53be8d8c67
Alex Zvoleff <azvoleff@gmail.com>
2014-03-12 18:27:26 -0400

    Remove aoi buffer option.

--- R/calc_gfc_tiles.R
+++ R/calc_gfc_tiles.R
@@ -10,20 +10,13 @@
 #' @param aoi an Area of Interest (AOI) as a \code{SpatialPolygons*} object.  
 #' If the AOI is not in the WGS84 geographic coordinate system, it will be 
 #' reprojected to WGS84.
-#' @param aoi_buffer a distance in meters to buffer the AOI by prior to 
-#' intersecting it with the GFC grid.
 #' @return a \code{SpatialPolygonsDataFrame} of the GFC tiles needed to cover 
 #' the AOI
 #' @examples
 #' tiles <- calc_gfc_tiles(test_poly)
 #' plot(tiles)
 #' plot(test_poly, lt=2, add=TRUE)
-calc_gfc_tiles <- function(aoi, aoi_buffer=0) {
-    if (aoi_buffer > 0) {
-        aoi_utm <- spTransform(aoi, CRS(utm_zone(aoi, proj4string=TRUE)))
-        aoi_utm <- gBuffer(aoi_utm, width=aoi_buffer, byid=TRUE)
-        aoi <- aoi_utm
-    }
+calc_gfc_tiles <- function(aoi) {
     aoi <- spTransform(aoi, CRS(proj4string(gfc_tiles)))
     intersecting <- as.logical(gIntersects(gfc_tiles, 
                                            gUnaryUnion(aoi), 
