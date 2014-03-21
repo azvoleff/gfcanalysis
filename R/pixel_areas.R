ab95d81d190578a73cc9de880fa84959167b48c5
Alex Zvoleff <azvoleff@gmail.com>
2014-03-21 15:01:49 -0400

    Output pixel areas by block to handle large rasters.

--- R/pixel_areas.R
+++ R/pixel_areas.R
@@ -9,15 +9,15 @@
 #' @param x a \code{Raster*} object
 #' @return \code{RasterLayer} with pixel areas (in meters)
 pixel_areas <- function(x) {
-    x <- raster(x, layer=1)
+    out <- raster(x)
     # Construct polygons for a single column of the raster
-    xleft <- xmin(x)
-    xright <- xmin(x) + xres(x)
+    xleft <- xmin(out)
+    xright <- xmin(out) + xres(out)
     # Note that ylower and yupper are setup so that the polygons are ordered
     # from high latitude to lowest latitude - needed because rasters are 
     # addressed starting from the upper left corner (highest latitude)
-    ylower <- seq(from=(ymax(x) - yres(x)), by=(-yres(x)), length.out=nrow(x))
-    yupper <- seq(from=ymax(x), by=(-yres(x)), length.out=nrow(x))
+    ylower <- seq(from=(ymax(out) - yres(out)), by=(-yres(out)), length.out=nrow(out))
+    yupper <- seq(from=ymax(out), by=(-yres(out)), length.out=nrow(out))
 
     poly_areas <- function(xl, xr, yl, yu) {
         areaPolygon(matrix(c(xl, yl,
@@ -26,9 +26,18 @@ pixel_areas <- function(x) {
                              xl, yu), ncol=2, byrow=TRUE))
     }
     areas <- mapply(poly_areas, xleft, xright, ylower, yupper)
+    
+    # Write areas by block to avoid running out of memory with very large 
+    # rasters
+    bs <- blockSize(out)
+    out <- writeStart(out, rasterTmpFile())
+    for (block_num in 1:bs$n) {
+        start_row <- bs$row[block_num]
+        end_row <- start_row + bs$nrows[block_num]
+        writeValues(out, rep(areas[start_row:end_row], each=ncol(out)), bs$row[block_num])
+    }
+    out <- writeStop(out)
+    names(out) <- 'area'
 
-    areas <- setValues(x, rep(areas, each=ncol(x)))
-    names(areas) <- 'area'
-
-    return(areas)
+    return(out)
 }
