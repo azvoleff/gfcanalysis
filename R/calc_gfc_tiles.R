14057920586446ef2fe68ff56853465308683a3d
Alex Zvoleff <azvoleff@gmail.com>
2014-09-17 12:29:47 -0400

    Fix imports.

--- R/calc_gfc_tiles.R
+++ R/calc_gfc_tiles.R
@@ -6,7 +6,7 @@
 #' @export
 #' @import rgdal
 #' @importFrom sp spTransform CRS proj4string
-#' @importFrom rgeos gBuffer gIntersects gUnaryUnion
+#' @importFrom rgeos gBuffer gIntersects gConvexHull
 #' @param aoi an Area of Interest (AOI) as a \code{SpatialPolygons*} object.  
 #' If the AOI is not in the WGS84 geographic coordinate system, it will be 
 #' reprojected to WGS84.
