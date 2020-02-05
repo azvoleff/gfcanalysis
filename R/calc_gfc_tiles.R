679c79bf0c3e5affb58df2e4122415ced0c77322
mcooper <mw.coop.r@gmail.com>
2020-02-04 19:12:17 -0500

    fix aoi boundary error

--- R/calc_gfc_tiles.R
+++ R/calc_gfc_tiles.R
@@ -1,12 +1,12 @@
 #' Calculate the GFC product tiles needed for a given AOI
 #'
-#' Intersects an (optionally buffered) AOI with the GFC 
+#' Intersects an AOI with the GFC 
 #' product grid to determine what tiles are need to cover the AOI.
 #'
 #' @export
 #' @import rgdal
 #' @importFrom sp spTransform CRS proj4string
-#' @importFrom rgeos gBuffer gIntersects gConvexHull
+#' @importFrom rgeos gOverlaps gConvexHull
 #' @param aoi an Area of Interest (AOI) as a \code{SpatialPolygons*} object.  
 #' If the AOI is not in the WGS84 geographic coordinate system, it will be 
 #' reprojected to WGS84.
@@ -18,9 +18,7 @@
 #' plot(test_poly, lt=2, add=TRUE)
 calc_gfc_tiles <- function(aoi) {
     aoi <- spTransform(aoi, CRS(proj4string(gfc_tiles)))
-    intersecting <- as.logical(gIntersects(gfc_tiles, 
-                                           gConvexHull(aoi), 
-                                           byid=TRUE))
+    intersecting <- as.logical(gOverlaps(gfc_tiles, gConvexHull(aoi), byid=TRUE))
     if (sum(intersecting) == 0) {
         stop('no intersecting GFC tiles found')
     } else {
